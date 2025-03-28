7a7abcb21b07d1ba01796e049b86b27a46e3601c
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-03-28 14:23:43 +0100

    update category model

--- src/models/CategoryModel.js
+++ src/models/CategoryModel.js
@@ -5,19 +5,12 @@ const categorySchema = new mongoose.Schema({
   name: { // Fält för att lagra kategorinamn (t.ex. "Skafferi", "Drycker")
     type: String, // Typen är String eftersom vi förväntar oss ett textvärde
     required: true, // Kategorin måste ha ett namn
-    unique: true, // Namnet på kategorin ska vara unikt
     trim: true, // Ta bort eventuella mellanslag före och efter namnet
   },
   description: { // Fält för en beskrivning av kategorin
     type: String, // Fältet är av typen String
     default: '', // Om ingen beskrivning anges, sätt standardvärdet till en tom sträng
   },
-  // Detta är för att hålla en lista av produkter som tillhör denna kategori. 
-  // Vi lagrar endast produktens ObjectId här (referens till Product-modellen)
-  products: [{ 
-    type: mongoose.Schema.Types.ObjectId, // Referens till Product-modellen
-    ref: 'Product', // Definierar att detta fält refererar till modellen "Product"
-  }],
 }, { 
   timestamps: true, // Skapar automatiskt fälten createdAt och updatedAt
 });
