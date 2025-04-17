fd9272b23d5882a86c72cb295b5eacf00ecc6091
Sandra Linnea <sandra.b93@hotmail.com>
2025-04-17 10:57:17 +0200

    Delete unnecessary comments

--- src/models/CategoryModel.js
+++ src/models/CategoryModel.js
@@ -1,20 +1,17 @@
-import mongoose from 'mongoose'; // Importera mongoose för att interagera med MongoDB
+import mongoose from 'mongoose';
 
-// Definiera schema för Category
 const categorySchema = new mongoose.Schema({
-  name: { // Fält för att lagra kategorinamn (t.ex. "Skafferi", "Drycker")
-    type: String, // Typen är String eftersom vi förväntar oss ett textvärde
-    required: true, // Kategorin måste ha ett namn
-    trim: true, // Ta bort eventuella mellanslag före och efter namnet
+  name: { 
+    type: String,
+    required: true,
+    trim: true,
   },
-  description: { // Fält för en beskrivning av kategorin
-    type: String, // Fältet är av typen String
-    default: '', // Om ingen beskrivning anges, sätt standardvärdet till en tom sträng
+  description: {
+    type: String,
+    default: '',
   },
 }, { 
-  timestamps: true, // Skapar automatiskt fälten createdAt och updatedAt
+  timestamps: true,
 });
 
-// Skapa Category-modellen baserat på schema
 export default mongoose.model('CategoryModel', categorySchema);
-// Exportera Category-modellen för att kunna använda den i andra filer
--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -1,13 +1,8 @@
-// routes/categoryRoutes.js
 import express from 'express';
 import { getCategories } from '../controllers/categoryController.js';
 
 const router = express.Router();
 
-// Route för att hämta alla kategorier
 router.get('/', getCategories);
 
 export default router;
-
-// Compare this snippet from src/controllers/productController.js:
-// // controllers/productController.js 
